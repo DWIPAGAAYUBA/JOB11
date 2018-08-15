4a6f155de0db0c874f43fa01c4d116fddd567b71
MSI <MSI@DESKTOP-DRHGVJJ>
2018-08-15 09:32:16 +0700

    Intial Commit

---
+++ src/J11/LATIHAN02.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package J11;
+
+import java.util.Scanner;
+
+public class LATIHAN02 {
+   private static void tampilJudul(String identitas){
+        System.out.println("Identitas : "+ identitas);
+        
+        System.out.println("\nConvert Kalimat Alay Angka (Vokal Ke Angka)\n");
+   }
+    public static void main(String[] args){
+    
+        String identitas = " Dwi paga ayuba / X RPL 2 / 10 ";
+        
+        tampilJudul(identitas);
+    }   
+}
